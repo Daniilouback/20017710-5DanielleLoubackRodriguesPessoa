7fa140a63ea1aff269e19694bc76bb68485d7a09
Danielle <danielleloubackpessoa@hotmai.com>
2021-08-25 23:11:02 -0300

    criação da pasta e do arq. dados.php

---
+++ dados/dados.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Doce Acaí</title>
+    </head>
+    <body>
+     
+    </body>
+</html>
+
+<?php
+$produtos = [
+    'acai'=>[ 
+      'descricao'=>" O mais puro açaí sem conservantes.", 
+      'preco'=> 20.00,
+      
+    ],
+    'acaic'=>[
+      'descricao'=>"Açaí com Nutella. #muuuuuitoamor.",
+      'preco'=> 25.99,
+      
+    ], 
+    'barca'=>[
+      'descricao'=>"	
+      
+      Açaí, 1x morango, 1x Ovomaltine, 1x Bis tradicional, 1x chantilly e 1x mms obs.: na barca de 1,8 litros vão dois acréscimos de cada, exceto: mms e morango.",
+      'preco'=> 60.99,
+      
+    ],
+  
+    'tacas'=>[
+      'descricao'=>"Uma delícia. Sobremesa perfeita...",
+      'preco'=> 29.99,
+      
+    ],
+    
+    
+  ];
+?>
